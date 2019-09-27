09e68a8b853eb50a654715df32f025f88f8c6d87
Roberto Medina <robby.medina562@gmail.com>
2019-09-27 08:46:27 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,12 @@
 class Customer
+  @@all = []
+  attr_accessor :name, :age
+  def initalize(name, age)
+    @name= name
+    @age = age
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
   
-end
